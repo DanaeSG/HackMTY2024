d9bdb81005bd0c097cba5b8798af4eb5aebc22c4
dhlabra <A01611973@tec.mx>
2024-09-14 23:29:59 -0600

    Gradle Update

--- CapitalOne/app/src/main/java/com/example/capitalone/views/upcoming_invoices.kt
+++ CapitalOne/app/src/main/java/com/example/capitalone/views/upcoming_invoices.kt
@@ -1,32 +1,16 @@
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Send
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.capitalone.R
-import com.example.capitalone.ui.theme.CO_Blue
 import com.example.capitalone.ui.theme.CO_Grey
 
 data class Invoice(
